bf9fe69056cef4aaf2c894819742634cb676119c
Yevgnen Koh <wherejoystarts@gmail.com>
2016-11-29 18:02:24 +0800

    Initialize.

---
+++ ivy-rich.el
@@ -0,0 +1,39 @@
+;;; ivy-rich.el ---
+
+;; Copyright (C) 2016 Yevgnen Koh
+
+;; Author: Yevgnen Koh <wherejoystarts@gmail.com>
+;; Package-Requires: ((emacs "24.1") (ivy "0.8.0"))
+;; Version: 0.0.1
+;; Keywords: ivy
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; More friendly interface (display transformer) for ivy.
+;;
+;; See documentation on https://github.com/yevgnen/ivy-rich.
+
+;;; Code:
+
+(require 'ivy-switch-buffer-rich)
+
+(defgroup ivy-rich nil
+  "More friendly interface (display transformer) for ivy."
+  :group 'ivy)
+
+(provide 'ivy-rich)
+
+;;; ivy-rich.el ends here
