c0a164ce42920df0629c87dad5ea1bddc79ceac3
Yevgnen Koh <wherejoystarts@gmail.com>
2020-11-25 08:58:57 +0800

    Update default transformers

--- ivy-rich.el
+++ ivy-rich.el
@@ -52,11 +52,11 @@
 (defcustom ivy-rich-display-transformers-list
   '(ivy-switch-buffer
     (:columns
-     ((ivy-switch-buffer-transformer (:width 30))
+     ((ivy-switch-buffer-transformer (:width 0.35))
       (ivy-rich-switch-buffer-size (:width 7))
       (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
       (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
-      (ivy-rich-switch-buffer-project (:width 15 :face success))
+      (ivy-rich-switch-buffer-project (:width 0.18 :face success))
       (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
      :predicate
      (lambda (cand) (get-buffer cand)))
@@ -66,20 +66,23 @@
       (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face))))
     counsel-M-x
     (:columns
-     ((counsel-M-x-transformer (:width 40))
+     ((counsel-M-x-transformer (:width 0.4))
       (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
     counsel-describe-function
     (:columns
-     ((counsel-describe-function-transformer (:width 40))
+     ((counsel-describe-function-transformer (:width 0.4))
       (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
     counsel-describe-variable
     (:columns
-     ((counsel-describe-variable-transformer (:width 40))
+     ((counsel-describe-variable-transformer (:width 0.4))
       (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))
     counsel-recentf
     (:columns
      ((ivy-rich-candidate (:width 0.8))
       (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))
+    counsel-bookmark
+    (:columns ((ivy-rich-bookmark-type)
+               (ivy-rich-bookmark-info)))
     package-install
     (:columns
      ((ivy-rich-candidate (:width 30))
@@ -454,7 +457,7 @@ or /a/…/f.el."
 ;; Possible setup:
 ;; counsel-projectile-switch-project
 ;; (:columns
-;;  ((ivy-rich-counsel-projectile-switch-project-project-name (:width 20 :face success))
+;;  ((ivy-rich-counsel-projectile-switch-project-project-name (:width 30 :face success))
 ;;   (ivy-rich-candidate)))
 (defun ivy-rich-counsel-projectile-switch-project-project-name (candidate)
   (or (projectile-project-name candidate) ""))
